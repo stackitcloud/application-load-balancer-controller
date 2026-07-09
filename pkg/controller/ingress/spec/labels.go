60459aa455f0c1b29c60103cbcd9a241fe7a2b15
Björn Fischer <bjoern.fischer@stackit.cloud>
2026-07-09 14:38:28 +0200

    Remove sanitize label function

--- pkg/controller/ingress/spec/labels.go
+++ pkg/controller/ingress/spec/labels.go
@@ -1,10 +1,5 @@
 package spec
 
-import (
-	"regexp"
-	"strings"
-)
-
 const (
 
 	// prefixALBIngressController is the prefix for all labels associated with ingress controllers
@@ -13,20 +8,3 @@ const (
 	// owned by a specific IngressClass.
 	LabelIngressClassUID = prefixALBIngressController + "ingress-class-uid"
 )
-
-// Replace non-alphanumeric characters (except '-', '_', '.') with '-'
-var reg = regexp.MustCompile(`[^-a-zA-Z0-9_.]+`)
-
-func SanitizeLabelValue(input string) string {
-	sanitized := reg.ReplaceAllString(input, "-")
-
-	// Ensure the label starts and ends with an alphanumeric character
-	sanitized = strings.Trim(sanitized, "-_.")
-
-	// Ensure the label is not longer than 63 characters
-	if len(sanitized) > 63 {
-		sanitized = sanitized[:63]
-	}
-
-	return sanitized
-}
